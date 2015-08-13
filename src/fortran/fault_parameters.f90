1f3e04b78040b19ab459b8ffdcdf91b994d0c55d
iwbailey <iainbailey@yahoo.co.uk>
2015-08-12 20:57:36 -0400

    Adjusted comments to be clearer
    Incorporated Arrhenius parameters

--- src/fortran/fault_parameters.f90
+++ src/fortran/fault_parameters.f90
@@ -1,18 +1,47 @@
 module fault_parameters
 
   !  UNITS: fault dimensions in km, time in year, velocities in mm/yr
-  real(kind=8), parameter :: Xlength = 70.0 ! length in km along strike, north of GH
-  real(kind=8), parameter :: Zdepth = 17.5  ! overall depth of fault region in km
-  integer, parameter :: nl = 128 ! number of cells along strike
-  integer, parameter :: nd = 32 ! number of cells over depth
-  !real(kind=8), parameter :: Xlength2 = 700.0 ! length  of extenal slip sections in km
-  real(kind=8), parameter :: fs = 0.75 ! static coefficient of friction
-  real(kind=8), parameter :: dos = 1.25 ! dynamic overshoot coeff
-  real(kind=8), parameter :: dtaumx = 60.0 ! maximum stress drop in bars
-  real(kind=8), parameter :: Vpl = 35.0 ! plate velocity in mm/yr
-  real(kind=8), parameter :: tauratioz = 4.0
-  real(kind=8), parameter :: zDB = 10.0 ! depth where creep rate at tau = fs*Seff equals Vpl
-  real(kind=8), parameter :: xDB = 7.5 ! analogous horizontal position of DB transition
-  real(kind=8), parameter :: t0 = 125.0
+
+  ! Length of the fault in km
+  real(kind=8), parameter :: Xlength = 70.0
+
+  ! Depth of the fault in km
+  real(kind=8), parameter :: Zdepth = 17.5
+
+  ! Number of slip cells along strike and depth
+  integer, parameter :: nl = 128
+  integer, parameter :: nd = 32
+
+  ! Static coefficient of friction
+  real(kind=8), parameter :: fs = 0.75
+
+  ! Dynamic overshoot coeff
+  real(kind=8), parameter :: dos = 1.25
+
+  ! Maximum stress drop in bars/cohesion
+  real(kind=8), parameter :: dtaumx = 60.0
+
+  ! Gradient of effective normal stress with depth
   real(kind=8), parameter :: dSigmaEff_dz = 180.0
+
+  ! Plate velocity in mm/yr
+  real(kind=8), parameter :: Vpl = 35.0
+
+  real(kind=8), parameter :: tauratioz = 4.0
+
+  ! Depth where creep rate at tau = fs*Seff equals Vpl
+  real(kind=8), parameter :: zDB = 10.0
+
+  ! Analogous horizontal position of DB transition in km
+  real(kind=8), parameter :: xDB = 7.5
+
+  ! Surface temperature in K
+  real(kind=8), parameter :: Tsurface = 273 + 20;
+
+  ! Temperature gradient with depth in deg/km
+  real(kind=8), parameter :: dTdz = 25.0
+
+  ! Gas constant in J/(mol K)
+  real(kind=8), parameter :: Rg = 8.3144
+
 end module fault_parameters

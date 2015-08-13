582f483022791d45580db8bad4b235355aefd652
iwbailey <iainbailey@yahoo.co.uk>
2015-08-12 20:51:50 -0400

    Added comments

--- src/fortran/add_randomness.f90
+++ src/fortran/add_randomness.f90
@@ -1,4 +1,18 @@
 subroutine add_randomness( crp, nl, nd, dx, dz, xDB, zDB, zcrpDB, zrcrp )
+  ! Add randomness to an existing distribution of creep parameters
+  !
+  !  add_randomness( crp, nl, nd, dx, dz, xDB, zDB, zcrpDB, zrcrp )
+  !
+  ! IN/OUT
+  ! crp = dimension (nl, nd) array of creep parameters
+  ! nl = number of cells along fault length
+  ! nd = number of cells down-dip
+  ! dx = along-length width of cell
+  ! dz = down-dip width of cell
+  ! xDB = creep boundary width from left and right edges
+  ! zDB = creep boundary depth from top of fault
+  ! zcrpDB =
+  ! zrcrp =
   implicit none
 
   integer, intent(in) :: nl, nd
@@ -18,29 +32,44 @@ subroutine add_randomness( crp, nl, nd, dx, dz, xDB, zDB, zcrpDB, zrcrp )
   ! Set the random seed
   call srand(12345)
 
+  ! Fault dimensions
+  faultDepth = nd*dz
+  faultLength = nl*dx
+
   ! Calculate depths at the center of each cell
   iDepth = (/ (i, i = 1,nd) /)
   zcell = (iDepth-0.5)*dz
+
+  ! Horizontal positions at center of each cell
   iStrike = (/ (i, i = 1,nl) /)
   xcell = (iStrike-0.5)*dx
-  faultDepth = nd*dz
-  faultLength = nl*dx
 
   ! Set the counters to zero
   l = 0
   k = 0
 
+  ! Loop through depths up to 13.5 km
   do j=1,nd
      if( zcell(j).lt.13.5) then
+
+        ! Loop through strike between 3.75 and 66.25
         do i=1,nl
            if( xcell(i).gt.3.75.and.xcell(i).lt.66.25) then
+
+              ! Get a random number
               rn = rand()
               k = k+1
+
+              ! Procede 20% of the time
               if(rn.le.0.2)then
                  l = l + 1
+
+                 ! Check if friction creep strength dominates
                  if( zcell(j).lt.zDB .and. xcell(i).gt.xDB .and. xcell(i).lt.(faultLength-xDB) ) then
+                    ! Brittle part
                     crp(i,j) = zcrpDB*exp(zrcrp*(11.25-zDB))
                  else
+                    ! Creep part
                     crp(i,j) = zcrpDB*exp(zrcrp*(8.75-zDB))
                  end if
               endif

b7e0f80bf3984695f903371d791c13d28ab4de18
iwbailey <iainbailey@gmail.com>
2018-06-21 21:57:17 +0100

    Fixed so it will work if current directory is not in the path, and if eog is not installed

--- test/test_loading/run_test.sh
+++ test/test_loading/run_test.sh
@@ -2,16 +2,24 @@
 #
 # Compile, run the test, plot the output
 
+set -e
+
 prog=test_loading_noheat
 
 echo "Compiling..."
 make $prog
 
 echo "Running program..."
-${prog} > ${prog}.out
+./${prog} > ${prog}.out
 
 echo "Plotting..."
 gnuplot < plot_${prog}.gp
 
 echo "Displaying"
-eog ${prog}.png
+if [ -x /usr/bin/eog ]; then
+    eog ${prog}.png
+elif [ -x /usr/bin/ristretto ]; then
+    ristretto $prog.png
+else
+    echo Output check plot written to $prog.png
+fi
